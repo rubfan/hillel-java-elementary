b460cc00d7efbd0a97dfcee62882a49586d0799d
EgorFomin <egorfomin97@gmail.com>
2018-02-21 22:15:48 +0200

    работа в классе lesson4

---
+++ task_2/egor.fomin.2.2/proj_lesson_4/src/ua/od/hillel/Main.java
@@ -0,0 +1,28 @@
+package ua.od.hillel;
+/**Project1_classLesson4*/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Boolean.TYPE + " " + Boolean.FALSE + " " + Boolean.TRUE);
+        System.out.println(Byte.TYPE + " " + Byte.SIZE + " от " + Byte.MIN_VALUE + " до " + Byte.MAX_VALUE);
+        System.out.println(Short.TYPE + " " + Short.SIZE + " от " + Short.MIN_VALUE + " до " + Short.MAX_VALUE);
+        System.out.println(Character.TYPE + " " + Character.SIZE + " от " + Character.MIN_VALUE + " до " + Character.MAX_VALUE);
+        System.out.println(Integer.TYPE + " " + Integer.SIZE + " от " + Integer.MIN_VALUE + " до " + Integer.MAX_VALUE);
+        System.out.println(Long.TYPE + " " + Long.SIZE + " от " + Long.MIN_VALUE + " до " + Long.MAX_VALUE);
+        System.out.println(Float.TYPE + " " + Float.SIZE + " от " + Float.MIN_VALUE + " до " + Float.MAX_VALUE);
+        System.out.println(Double.TYPE + " " + Double.SIZE + " от " + Double.MIN_VALUE + " до " + Double.MAX_VALUE);
+        /**Hot keys:
+         * Ctrl + d - копировать строку
+         * Ctrl + Alt + S - настройки
+         *
+         *
+         */
+
+
+
+
+    /*public void vasyaLubitPivo(){
+        //.....
+    }*/
+    }
+}
