0aa944e25b6d820383fabfbdaafe91422ccf2a19
Slava.1.5 <vve1991@gmail.com>
2018-02-21 21:24:06 +0200

    project directories + lesson_4 changes

---
+++ task_2/slava.yevtodii.1.5/proj_lesson-4/src/ua/od/hillel/Main.java
@@ -0,0 +1,12 @@
+package ua.od.hillel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello guys");
+	}
+
+	public void lesson4Method {
+
+    }
+}
