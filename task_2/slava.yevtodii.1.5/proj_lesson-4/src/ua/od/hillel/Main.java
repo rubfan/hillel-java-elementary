c1f58d14fcef9d3a824fa3917206a7094ae4c970
Slava.1.5 <vve1991@gmail.com>
2018-02-21 21:53:23 +0200

    added java type project during lesson 4

--- task_2/slava.yevtodii.1.5/proj_lesson-4/src/ua/od/hillel/Main.java
+++ task_2/slava.yevtodii.1.5/proj_lesson-4/src/ua/od/hillel/Main.java
@@ -3,10 +3,12 @@ package ua.od.hillel;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello guys");
-	}
-
-	public void lesson4Method {
-
+        System.out.println(Boolean.TYPE+" "+Boolean.FALSE+" "+Boolean.TRUE);
+        System.out.println(Byte.TYPE+" "+Byte.SIZE+" from "+Byte.MIN_VALUE+" to "+Byte.MAX_VALUE);
+        System.out.println(Character.TYPE+" "+Character.SIZE+" from "+Character.MIN_VALUE+" to "+Character.MAX_VALUE);
+        System.out.println(Integer.TYPE+" "+Integer.SIZE+" from "+Integer.MIN_VALUE+" to "+Integer.MAX_VALUE);
+        System.out.println(Long.TYPE+" "+Long.SIZE+" from "+Long.MIN_VALUE+" to "+Long.MAX_VALUE);
+        System.out.println(Float.TYPE+" "+Float.SIZE+" from "+Float.MIN_VALUE+" to "+Float.MAX_VALUE);
+        System.out.println(Double.TYPE+" "+Double.SIZE+" from "+Double.MIN_VALUE+" to "+Double.MAX_VALUE);
     }
 }
