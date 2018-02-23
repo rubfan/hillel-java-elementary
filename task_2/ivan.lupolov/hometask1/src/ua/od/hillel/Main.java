38d698ed2f476491bd716d379925f6e4768660ac
lupolov-ivan <ivannaib2012@gmail.com>
2018-02-23 22:57:22 +0200

    create new project

---
+++ task_2/ivan.lupolov/hometask1/src/ua/od/hillel/Main.java
@@ -0,0 +1,13 @@
+package ua.od.hillel;
+
+/**
+ * Home task for Lesson 2
+ * @author Ivan Lupolov
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
