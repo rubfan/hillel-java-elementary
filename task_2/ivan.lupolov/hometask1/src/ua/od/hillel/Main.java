3641422af4848e5b477969622e283f02f4c7ade4
lupolov-ivan <ivannaib2012@gmail.com>
2018-02-23 23:02:06 +0200

    add method minAndMaxNumbers()

--- task_2/ivan.lupolov/hometask1/src/ua/od/hillel/Main.java
+++ task_2/ivan.lupolov/hometask1/src/ua/od/hillel/Main.java
@@ -23,4 +23,21 @@ public class Main {
 
         return min;
     }
+
+    /** 2) Find the minimum and maximum of the three numbers  */
+    public static String minAndMaxNumbers(int num1, int num2, int num3) {
+        int min = num1;
+        int max = num2;
+
+        if (min > num2) {
+            min = num2;
+            max = num1;
+        } else if (min > num3){
+            min = num3;
+        } else if(max < num3) {
+            max = num3;
+        }
+
+        return min + " " + max;
+    }
 }
