417b1e2c8308e310bc37a7adb42ce31033c3df1a
lupolov-ivan <ivannaib2012@gmail.com>
2018-02-23 23:00:36 +0200

    add method minOfThreeNumbers()

--- task_2/ivan.lupolov/hometask1/src/ua/od/hillel/Main.java
+++ task_2/ivan.lupolov/hometask1/src/ua/od/hillel/Main.java
@@ -10,4 +10,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    /** 1) Find the minimum of the three numbers  */
+    public static int minOfThreeNumbers(int num1, int num2, int num3) {
+        int min = num1;
+
+        if (min > num2) {
+            min = num2;
+        } else if (min > num3){
+            min = num3;
+        }
+
+        return min;
+    }
 }
