d46252e6da00615771bfa545d3db7e6a0da61431
lupolov-ivan <ivannaib2012@gmail.com>
2018-02-26 14:43:00 +0200

    Merge branch 'master' of https://github.com/rubfan/hillel-java-elementary
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ task_1/slava.yevtodii.1.5/src/FirstTask.java
@@ -0,0 +1,37 @@
+/**
+* My first java file
+*/
+
+public class FirstTask {
+
+	/**
+	*
+	* @param args command line document.
+	*
+	*/
+
+	public static void main(String[] args) {
+
+		// I think we gonna print something out
+
+		System.out.println("This is my first task on Java Elementary!");
+	}
+
+	/**
+	* dividing 2 values
+	* @param x first value
+	* @param y second value
+	* @return dividing
+	* @since 1.0
+	*/
+	
+	public double plus(int x, int y) {
+
+	/*
+		this is just comment
+		what's up man?
+	*/
+		return x/y;
+	}
+
+}
