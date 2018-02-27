0c53ae1827abadec693dbebc3b39fe64445c210f
ruslana1319 <ruslana1319.d@gmail.com>
2018-02-27 22:19:44 +0200

    wip

--- lessons/lesson_2/Homework1.java
+++ lessons/lesson_2/Homework1.java
@@ -15,40 +15,39 @@ public class Homework1{
   }
 /**find minimum of three numbers */
   public static void minOfThreeNumbers(int n1, int n2, int n3){
-  int min= Math.min(n1, n2); 
-  min= Math.min(min, n3);
-  System.out.println("min:" + min + "from:"+ n1+"," + n2+"," + n3+";"); 
+    int min= Math.min(n1, n2); 
+    min= Math.min(min, n3);
+      System.out.println("min:" + min + "from:"+ n1+"," + n2+"," + n3+";"); 
   }
 /**find min and max of three numbers */
   public static void minAndMaxOfThreeNumbers(int n1, int n2, int n3){
-   int min= Math.min(n1, n2); 
+    int min= Math.min(n1, n2); 
     min= Math.min(min, n3); 
     
     int max= Math.max(n1, n2); 
     max= Math.max(max, n3);
-    
-  System.out.println("min:"+ min+", max:" + max+"from:"+ n1+","+ n2+","+ n3+";");
+      System.out.println("min:"+ min+", max:" + max+"from:"+ n1+","+ n2+","+ n3+";");
   }
 //**find all min of three numbers */   
   public static void allMinOfThreeNumbers(int[] mas1){ 
      int max =mas1[0];
-      for (int i=0; i<mas1.length; i++){
-      if(mas1[i]>max){
-      max = mas1[i];
-            }
-      }
-      for (int i=0; i<mas1.length; i++){
-        if(mas1[i]<max){ 
-        System.out.println(mas1[i]+ " is less that max "+ max);
+        for (int i=0; i<mas1.length; i++){
+          if(mas1[i]>max){
+              max = mas1[i];
+           }
         }
-      }
+        for (int i=0; i<mas1.length; i++){
+          if(mas1[i]<max){ 
+            System.out.println(mas1[i]+ " is less that max "+ max);
+          }
+        }
   }  
 //*check parity */
   public static void parityOfNumber(double n){
     if (n%2==0){
-    System.out.println("number"+ n+ "is even");
+      System.out.println("number"+ n+ "is even");
     }else{
-    System.out.println("number"+ n+ "is odd");
+      System.out.println("number"+ n+ "is odd");
     }
   }
  //is the result of division even
@@ -70,19 +69,19 @@ public class Homework1{
       }
     }
   //* payment for flat */
-    public static void paymentForFlat(int numOfDays,int numOfDaysFor1discount, int numOfDaysFor2discount, int costFor1Day, int discount1, int discount2){
-      int payment=costFor1Day*numOfDays;
+   public static void paymentForFlat(int numOfDays,int numOfDaysFor1discount, int numOfDaysFor2discount, int costFor1Day, int discount1, int discount2){
+     int payment=costFor1Day*numOfDays;
       
-      if(numOfDays<=numOfDaysFor2discount){
-        payment-=discount1;
+        if(numOfDays<=numOfDaysFor2discount){
+          payment-=discount1;
           
-      }if(numOfDays>numOfDaysFor2discount){
-         payment-=discount2;
+        }if(numOfDays>numOfDaysFor2discount){
+          payment-=discount2;
        
-      }
-      System.out.println("payment for flat= "+ payment); 
-      }
-    } 
+        }
+          System.out.println("payment for flat= "+ payment); 
+     }
+ } 
    
 
  
