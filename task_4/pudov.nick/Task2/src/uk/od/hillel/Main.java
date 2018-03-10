8788bbbc18191ce80b7c3277fdf0761ef3c88b23
ant1xr1s <at1xr1s@gmail.com>
2018-03-10 18:58:17 +0200

    task 1-6 lesson 4

---
+++ task_4/pudov.nick/Task2/src/uk/od/hillel/Main.java
@@ -0,0 +1,15 @@
+package uk.od.hillel;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int a=1;
+        for (int i=1; i < 56; i++)
+        {
+            System.out.println(a);
+            a+=2;
+        }
+
+    }
+}
