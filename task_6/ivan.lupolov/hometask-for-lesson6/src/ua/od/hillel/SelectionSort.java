c8380949a6ab1defa13545ba5e9211f8278175a4
lupolov-ivan <ivannaib2012@gmail.com>
2018-03-19 00:27:13 +0200

    add selection sort method

---
+++ task_6/ivan.lupolov/hometask-for-lesson6/src/ua/od/hillel/SelectionSort.java
@@ -0,0 +1,38 @@
+package ua.od.hillel;
+
+public class SelectionSort {
+
+
+    public static void main(String[] args) {
+
+        int[] sortArray = selectionSort(new int[]{1,84,0,8,73,457,-84,3,-7,3});
+
+        for (int i = 0; i < sortArray.length; i++) {
+            System.out.print(sortArray[i] + " ");
+        }
+    }
+
+    public static int[] selectionSort(int[] array) {
+
+        int min;
+        int index;
+        int tmp;
+
+
+        for (int i = 0; i < array.length; i++) {
+            min = array[i];
+            index = i;
+            for (int j = i; j < array.length; j++) {
+                if(array[j] < min) {
+                    min = array[j];
+                    index = j;
+                }
+            }
+            tmp = array[i];
+            array[i] = min;
+            array[index] = tmp;
+        }
+
+        return array;
+    }
+}
